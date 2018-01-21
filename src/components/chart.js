80b41a2c8057f55464f9090042aa8c5179997425
chrisziegler <chrisrziegler@gmail.com>
2018-01-20 21:51:29 -0800

    Added google map api to Sparklines charts table rows

--- src/components/chart.js
+++ src/components/chart.js
@@ -12,7 +12,7 @@ export default (props) => {
         <SparklinesLine color={props.color} />
         <SparklinesReferenceLine type="avg" />
       </Sparklines>
-      <div>{average(props.data)}</div>
+      <div><strong>{average(props.data)} {props.units}</strong></div>
     </div>
   )
 }
--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 
 class WeatherList extends Component {
@@ -14,13 +15,14 @@ class WeatherList extends Component {
     const temps = cityData.list.map((item) => item.main.temp);
     const pressures = cityData.list.map((item) => item.main.pressure);
     const humidities = cityData.list.map((item) => item.main.humidity);
+    const { lat, lon } = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
-        <td><Chart data={temps} color="orange" /></td>
-        <td><Chart data={pressures} color="green" /></td>
-        <td><Chart data={humidities} color="black" /></td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color="orange" units="&#8457;" /></td>
+        <td><Chart data={pressures} color="green" units="hPa"/></td>
+        <td><Chart data={humidities} color="black" units="%"/></td>
       </tr>
     );
   }
